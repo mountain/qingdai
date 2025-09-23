7ad4ce040687d12ad12c34268ff080da19b377c9
mingli <mingli.yuan@gmail.com>
2025-09-23 11:07:00 +0800

    fix greenhouse factor, distance and lum

--- pygcm/constants.py
+++ pygcm/constants.py
@@ -15,20 +15,20 @@ L_SUN = 3.828e26     # Luminosity of the Sun (W)
 AU = 1.496e11        # Astronomical Unit (m)
 
 # --- Harmony Star System Parameters (from docs/01-astronomical-setting.md) ---
-# Star A (G2V)
-M_A = 1.0 * M_SUN
-L_A = 1.0 * L_SUN
+# Star A (G6V)
+M_A = 0.946 * M_SUN
+L_A = 0.8 * L_SUN
 
 # Star B (K1V)
 M_B = 0.8 * M_SUN
-L_B = 0.4 * L_SUN
+L_B = 0.410 * L_SUN
 
 # Binary System
 M_TOTAL_STARS = M_A + M_B
 A_BINARY = 0.5 * AU  # Semi-major axis of the binary pair
 
 # --- Qingdai Planet Parameters ---
-A_PLANET = 1.5 * AU     # Semi-major axis of the planet's orbit (final, final tuning)
+A_PLANET = 1.45 * AU     # Semi-major axis of the planet's orbit (final, final tuning)
 PLANET_RADIUS = 6.371e6  # Planet radius (m, Earth-like for now)
 PLANET_ALBEDO = 0.3      # Planetary albedo (Earth-like for now)
 PLANET_OMEGA = 8.726646259971648e-5  # Angular velocity (rad/s, for a 20-hour day)
